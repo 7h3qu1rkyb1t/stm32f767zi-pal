245606a1b1998357a361a17e78ae282feb6f4d70
hardfau18 <the.qu1rky.b1t@gmail.com>
2020-12-15 10:05:33 +0530

    blink project complete

--- projects/blink/src/main.c
+++ projects/blink/src/main.c
@@ -7,5 +7,8 @@ int arr[3]={1,2,3};
 int main(){
     RCC->AHB1ENR_b.GPIOBEN = SET;
     GPIOB->MODER_b.MODER7 = 1;
-    GPIOB->ODR_b.ODR7 = SET;
+    while(1){
+        GPIOB->ODR_b.ODR7 ^= SET;
+        for(int i=0; i<1000000; i++);
+    }
 }
